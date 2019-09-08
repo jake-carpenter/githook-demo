eea88c82be39c4db8559895f0ca5ef5d8f95367f
Jake Carpenter <jake@jakecarpenter.dev>
2019-09-08 14:31:36 -0600

    Add dotnet-script based push script

--- GitHookDemo/Program.cs
+++ GitHookDemo/Program.cs
@@ -7,6 +7,8 @@ namespace GitHookDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("test");
+            d
         }
     }
 }
