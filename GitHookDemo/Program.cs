6e9261a1bb5938ad8c18baab1151e38346ecaa8e
Jake Carpenter <jake@jakecarpenter.dev>
2019-09-07 21:38:55 -0600

    Initial commit

---
+++ GitHookDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHookDemo
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
